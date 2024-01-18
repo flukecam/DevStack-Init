70cdae42a88ee9f2a57b4c689f990515c6a94de1
flukecam <peeraphatbunluea@gmail.com>
2024-01-18 11:48:16 +0700

    Update devstack-init.sh

--- devstack-init.sh
+++ devstack-init.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-tmux new-session -d -s devstack-init "curl https://raw.githubusercontent.com/flukecam/DevStack-Init/main/test-script.sh | sh"
+echo "TRY TO INIT DEVSTACK"
+
+git clone https://github.com/openstack/devstack.git ~/devstack
+
+cd ~/devstack && git checkout stable/2023.2
+
+sudo ~/devstack/tools/create-stack-user.sh
+
+sudo rm -rf /opt/stack
+
+cat << EOF > ~/devstack/local.conf
+[[local|localrc]]
+ADMIN_PASSWORD=k76iLgK4zVWDtUYLLytZ
+DATABASE_PASSWORD=1fZJo3kWf4RgqeohsI8Y
+RABBIT_PASSWORD=QSY2B0S817AAY902Ct3b
+SERVICE_PASSWORD=Zr4mUM9eU03Q7c8arEnt
+EOF
+
+tmux new-session -d -s devstack-init "~/devstack/stack.sh"
