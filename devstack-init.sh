89e9bd2196067239edc9cb9acc45daa9599e3332
flukecam <peeraphatbunluea@gmail.com>
2025-08-01 16:43:22 +0700

    Update devstack-init.sh

--- devstack-init.sh
+++ devstack-init.sh
@@ -10,9 +10,12 @@ sudo ~/devstack/tools/create-stack-user.sh
 
 sudo rm -rf /opt/stack
 
+externalip=$(curl 169.254.169.254/latest/meta-data/public-ipv4)
+internalip=$(curl 169.254.169.254/latest/meta-data/local-ipv4)
+
 cat << EOF > ~/devstack/local.conf
 [[local|localrc]]
-HOST_IP=127.0.0.1
+HOST_IP=$internalip
 SERVICE_HOST=$HOST_IP
 FLOATING_RANGE=10.58.1.192/27
 FIXED_RANGE=10.11.12.0/24
@@ -21,6 +24,16 @@ ADMIN_PASSWORD=k76iLgK4zVWDtUYLLytZ
 DATABASE_PASSWORD=1fZJo3kWf4RgqeohsI8Y
 RABBIT_PASSWORD=QSY2B0S817AAY902Ct3b
 SERVICE_PASSWORD=Zr4mUM9eU03Q7c8arEnt
+
+# Nova 
+NOVNCPROXY_URL="http://$externalip:6080/vnc_lite.html
+LIBVIRT_TYPE=qemu
+
+# Swift
+enable_service s-proxy s-object s-container s-account
+SWIFT_HASH=66a3d6b56c1f479c8b4e70ab5c2000f5
+SWIFT_REPLICAS=1
+SWIFT_DATA_DIR=$DEST/data
 EOF
 
 tmux new-session -d -s devstack-init "~/devstack/stack.sh"
